b0a22c82c85dc949bf19c57ef321083cf52c2f98
Mauricio Pasquier Juan <mauricio@gauss.localdomain>
2011-09-06 17:08:30 -0300

    commit inicial rails 3.1.0 y gemas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suelos::Application.config.secret_token = '499f90aeb5115493eaca733fd628d85ce52f1147a5235cad2a65eab63cad8e2078100756994fd83f0a832b404b790eb5ed630cd2eb9f8db22f066228939e5691'
