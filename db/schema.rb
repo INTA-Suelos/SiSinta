da32f62c4e95395aa9985df3f5f61799a49a2bdc
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2011-09-13 06:14:12 -0300

    esquema después de las migraciones

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110913091043) do
+
+  create_table "horizontes", :force => true do |t|
+    t.integer  "profundidad"
+    t.string   "color_seco"
+    t.string   "color_humedo"
+    t.string   "tipo"
+    t.string   "clase"
+    t.string   "grado"
+    t.float    "ph"
+    t.string   "textura"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "observacion_id"
+  end
+
+  create_table "observaciones", :force => true do |t|
+    t.date     "fecha"
+    t.string   "numero"
+    t.boolean  "modal"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
