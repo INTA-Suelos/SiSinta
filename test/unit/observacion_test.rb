731f146c92ab85cac62907438e647b69d18d5230
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2011-09-13 07:45:54 -0300

    test de fecha

--- test/unit/observacion_test.rb
+++ test/unit/observacion_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ObservacionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  fixtures :all
+
+  test "la fecha no puede ser del futuro" do
+    o = Observacion.new :fecha => Date.today + 2
+    assert_nil(h, "si se está cargando la observación es porque ya pasó")
+  end
+
 end
