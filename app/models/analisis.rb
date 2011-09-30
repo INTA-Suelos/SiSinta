7659ef145468abdbd5fe781c0dcf694c1ac4a8eb
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2011-09-30 11:56:26 -0300

    modelo Analisis.Texturas

--- app/models/analisis.rb
+++ app/models/analisis.rb
@@ -1,3 +1,4 @@
 class Analisis < ActiveRecord::Base
+  has_one :textura, :dependent => :destroy
   belongs_to :horizonte
 end
--- app/models/horizonte.rb
+++ app/models/horizonte.rb
@@ -2,7 +2,9 @@
 class Horizonte < ActiveRecord::Base
   has_one :analisis,      :dependent => :destroy
   has_one :color,         :dependent => :destroy
+  has_one :limite,        :dependent => :destroy
   has_one :consistencia,  :dependent => :destroy
   has_one :estructura,    :dependent => :destroy
+  has_many :texturas, :through => :analisis
   belongs_to :observacion
 end
