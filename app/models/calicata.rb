2f627d0c54e08a28e873f0cb8e2138d5ea15d691
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2011-10-13 18:16:54 -0300

    algunas validaciones

--- app/models/calicata.rb
+++ app/models/calicata.rb
@@ -1,6 +1,10 @@
 # -*- encoding : utf-8 -*-
 class Calicata < ActiveRecord::Base
   validate :la_fecha_no_puede_ser_futura
+  validates :escurrimiento, :inclusion => { :in => @@valores_escurriemiento,
+    :message => "%{value} no es un grado de escurrimiento válido" }
+  validates :pendiente, :inclusion => { :in => @@valores_pendiente,
+    :message => "%{value} no es una clase de pendiente válida" }
 
   has_many :horizontes,     :dependent => :destroy
   has_many :fotos,          :dependent => :destroy
@@ -20,4 +24,7 @@ class Calicata < ActiveRecord::Base
       errors.add(:fecha, I18n.t("error por fecha futura"))
     end
   end
+
+  @@valores_escurrimiento = ['estancado','muy lento','lento','medio','rápido','muy rápido']
+  @@valores_pendiente = ['0 - 1%','1 - 3%','3 - 10%','10 - 25%','25 - 45%','45%']
 end
