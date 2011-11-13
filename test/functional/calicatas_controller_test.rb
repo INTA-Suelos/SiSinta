6955b3834f736ddf820b56d3405651b5f01875cc
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2011-11-13 00:57:53 -0300

    mejoro los tests

--- test/functional/calicatas_controller_test.rb
+++ test/functional/calicatas_controller_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class CalicatasControllerTest < ActionController::TestCase
 
   setup do
-    @calicata = calicatas(:one)
-    sign_in usuarios(:one)
+    @calicata = calicatas(:uno)
+    sign_in usuarios(:uno)
   end
 
   test "should get index" do
@@ -19,7 +19,9 @@ class CalicatasControllerTest < ActionController::TestCase
   end
 
   test "should create calicata" do
-    assert_difference('Calicata.count') do
+    @request.env['HTTP_REFERER'] = 'http://localhost:3000/calicatas/'
+
+    assert_difference('Calicata.count', 1, @calicata.errores) do
       post :create, calicata: @calicata.attributes
     end
 
@@ -27,21 +29,32 @@ class CalicatasControllerTest < ActionController::TestCase
   end
 
   test "should show calicata" do
+    @request.env['HTTP_REFERER'] = '/calicatas'
+
     get :show, id: @calicata.to_param
+
     assert_response :success
   end
 
   test "should get edit" do
+    @request.env['HTTP_REFERER'] = "http://localhost:3000/calicatas/edit/#{@calicata.to_param}"
+
     get :edit, id: @calicata.to_param
+
     assert_response :success
   end
 
   test "should update calicata" do
+    @request.env['HTTP_REFERER'] = "http://localhost:3000/calicatas/#{@calicata.to_param}"
+
     put :update, id: @calicata.to_param, calicata: @calicata.attributes
+
     assert_redirected_to calicata_path(assigns(:calicata))
   end
 
   test "should destroy calicata" do
+    @request.env['HTTP_REFERER'] = 'http://localhost:3000/calicatas/'
+
     assert_difference('Calicata.count', -1) do
       delete :destroy, id: @calicata.to_param
     end
--- test/unit/calicata_test.rb
+++ test/unit/calicata_test.rb
@@ -5,9 +5,38 @@ class CalicataTest < ActiveSupport::TestCase
 
   fixtures :all
 
+  test "debería prohibir guardar calicatas sin fecha" do
+    c = Calicata.new
+    assert c.invalid?, "falta definir la fecha"
+  end
+
   test "debería prohibir fechas del futuro" do
-    o = Calicata.new :fecha => Date.today + 2
-    assert(!o.valid?, "se guardó una fecha que no ha llegado")
+    c = Calicata.new :fecha => Date.today + 2
+    assert c.invalid?, "la fecha es del futuro"
+  end
+
+  test "debería crear la calicata y objetos asociados" do
+    atributos = { :fecha => Date.today,
+                  :serie_attributes => {
+                    :simbolo => "alguna serie" },
+                  :paisaje_attributes => {},
+                  :horizontes_attributes => [{}],
+                  :fotos_attributes => [{}],
+                  :capacidad_attributes => {
+                    :subclases_attributes => [
+                      {:codigo => 's'}],
+                    :clase_attributes => {
+                      :codigo => 'codigo'}}
+                }
+    c = Calicata.new(atributos)
+    assert c.valid?, c.errors.full_messages.join(', ')
+    assert_not_nil c.serie, "no se creó la serie"
+    assert_not_nil c.paisaje, "no se creó el paisaje"
+    assert_not_nil c.fotos, "no se crearon fotos"
+    assert_not_nil c.horizontes, "no se crearon la horizontes"
+    assert_not_nil c.capacidad, "no se creó la capacidad"
+    assert_match /alguna serie/, c.serie.simbolo
+    assert_match /codigo/, c.capacidad.clase.codigo
   end
 
 end
