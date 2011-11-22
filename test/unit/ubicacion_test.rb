32288e9aecb92ca8d5a6062eb2cfdabc25c514da
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2011-11-21 21:18:24 -0300

    ubicación más testeada

--- test/unit/ubicacion_test.rb
+++ test/unit/ubicacion_test.rb
@@ -15,4 +15,12 @@ class UbicacionTest < ActiveSupport::TestCase
     assert Ubicacion.new(:coordenadas => "sarasa").invalid?, "permite guardar 'sarasa' como latitud/longitud"
   end
 
+  test "debería validar latitud;longitud sólo entre -90/90 y -180/180" do
+    regex = Ubicacion::EPSG_4326
+    assert_match regex, '90;180', "no permite 90;180"
+    assert_match regex, '89.000000;120.000000', "no permite 89.000000;120.000000"
+    assert_match regex, '181.00;120.00', "permite 181.00;120.00"
+    assert_match regex, '-90.00001;-180.0000', "permite -90.00001;-180.0000"
+  end
+
 end
