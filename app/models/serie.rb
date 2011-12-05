09bbf62b7bfe9e5be94608943cd41f9e08188930
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2011-12-05 18:41:29 -0300

    devuelvo la calicata modal de la serie

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,5 +1,9 @@
 class Serie < ActiveRecord::Base
-  has_many :calicatas, :inverse_of => :serie
+  has_many :calicatas, :inverse_of => :serie do
+    def modal
+      where('modal = ?', true).first
+    end
+  end
 
   validates_uniqueness_of :simbolo, :allow_blank => true
   validates_uniqueness_of :nombre
