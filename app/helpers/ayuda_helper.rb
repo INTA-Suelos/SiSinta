7cad6893f7638da9af136776536175b79f4ae77f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2011-12-28 04:07:25 -0300

    helper para hacer div de ayuda y sus tests

---
+++ app/helpers/ayuda_helper.rb
@@ -0,0 +1,24 @@
+# -*- encoding : utf-8 -*-
+module AyudaHelper
+
+  # Crea los divs preparados para el tooltip de los formularios, con la
+  # descripción del campo.
+  #
+  # * *Args*    :
+  #   - +modelo+ -> El modelo al que pertenece el campo.
+  #   - +campo+ -> El campo del que devolver la descripción.
+  #   - +html+ -> Hash de opciones para el div. Se usa +clases+ para determinar
+  #   la/las clase/s del +div+, que por default es +ayuda+, e +id+ para
+  #   determinar el id, que por default es +ayuda_modelo_campo+
+  # * *Returns* :
+  #   - Un +div+ preparado con la clase 'ayuda'
+  #
+  def ayuda_para(modelo, campo, html = {})
+    # Hay que usar valor3 y valor2 porque AR no detecta los alias, todavía
+    texto = Ayuda.where(:valor3 => modelo, :valor2 => campo).first.descripcion
+    id = html[:id] || "ayuda_#{modelo}_#{campo}"
+    clases = html[:clases] || 'ayuda'
+
+    "<div id='#{id}' class='#{clases}'>#{texto}</div>".html_safe
+  end
+end
--- test/unit/helpers/usuarios_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class UsuariosHelperTest < ActionView::TestCase
-end
