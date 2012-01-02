d7bc2f80fc0110a997898fb06e482b3a628f60db
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-01-02 17:25:01 -0300

    tests correctos para paisaje

--- app/models/paisaje.rb
+++ app/models/paisaje.rb
@@ -2,4 +2,5 @@ class Paisaje < ActiveRecord::Base
   belongs_to :calicata, :inverse_of => :paisaje
 
   validates_presence_of :calicata
+  validates_presence_of :tipo, :forma, :simbolo
 end
--- test/unit/paisaje_test.rb
+++ test/unit/paisaje_test.rb
@@ -5,12 +5,12 @@ class PaisajeTest < ActiveSupport::TestCase
 
   fixtures :calicatas
 
-  test "debería negarse a guardarla si los campos son nulos" do
-    assert !Paisaje.new.save, "permite guardar un paisaje vacío"
+  test "debería negarse a guardarla sin calicata asociada" do
+    assert !Paisaje.new.save, "permite guardar un paisaje sin calicata asociada"
   end
 
-  test "debería negarse a guardar un paisaje sin datos" do
-    flunk
+  test "debería negarse a guardar un paisaje con datos vacíos" do
+    assert !Paisaje.new(:calicata => calicatas(:uno)).save, "permite guardar un paisaje sin datos"
   end
 
 end
