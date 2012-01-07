92febe31e06d754962ca243b18e5e22901244533
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-01-06 21:15:03 -0300

    evito crear fases y grupos vacíos

--- app/models/calicata.rb
+++ app/models/calicata.rb
@@ -1,12 +1,13 @@
 # -*- encoding : utf-8 -*-
 class Calicata < ActiveRecord::Base
   after_initialize :preparar
+  before_validation :limpiar
 
   scope :series, where(:modal => 'true')
 
   validate :la_fecha_no_puede_ser_futura
   validates_presence_of :fecha
-  validates_uniqueness_of :nombre, :allow_blank => true
+  validates_uniqueness_of :nombre, :numero, :allow_blank => true
   validates_presence_of :nombre, :simbolo, :if => Proc.new { |c| c.modal? }
   validates_numericality_of :cobertura_vegetal, :only_integer => true, :allow_nil => true,
                             :greater_than => 0, :less_than => 101
@@ -15,8 +16,7 @@ class Calicata < ActiveRecord::Base
   has_many :fotos,        :dependent => :destroy, :inverse_of => :calicata
   has_one :capacidad,     :dependent => :destroy, :inverse_of => :calicata
   has_one :ubicacion,     :dependent => :destroy, :inverse_of => :calicata
-
-  has_one :paisaje,       :inverse_of => :calicata
+  has_one :paisaje,       :dependent => :destroy, :inverse_of => :calicata
 
   #
   # Tablas de lookup. Las asociaciones 1 a 1 pueden ser:
@@ -40,12 +40,11 @@ class Calicata < ActiveRecord::Base
   has_many :limites,        :through => :horizontes
 
   belongs_to :usuario, :inverse_of => :calicatas
-  belongs_to :fase, :inverse_of => :calicatas, :autosave => true
-  belongs_to :grupo, :inverse_of => :calicatas, :autosave => true
+  belongs_to :fase, :inverse_of => :calicatas
+  belongs_to :grupo, :inverse_of => :calicatas
 
   accepts_nested_attributes_for :capacidad, :paisaje, :horizontes, :fase, :ubicacion,
-                                :grupo,
-                                :reject_if => :all_blank
+                                :grupo
 
 # == Validaciones
 
@@ -60,6 +59,23 @@ class Calicata < ActiveRecord::Base
 
   def preparar
     super(%w{capacidad paisaje ubicacion fase grupo})
+  end
+
+  #
+  # Arregla la entrada para que no haya objetos repetidos ni se creen vacíos
+  #
+  def limpiar
+    if self.grupo.try(:descripcion).present? then
+      self.grupo = Grupo.find_or_create_by_descripcion self.grupo.descripcion
+    else
+      self.grupo = nil
+    end
+
+    if self.fase.try(:nombre).present? then
+      self.fase = Fase.find_or_create_by_nombre self.fase.nombre
+    else
+      self.fase = nil
+    end
   end
 
 end
