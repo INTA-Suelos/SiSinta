d0701058ae6eef30a55ff0b20e457190b3f7814a
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-04-26 07:09:27 -0300

    actualizo tests

--- test/functional/grupo_controller_test.rb
+++ test/functional/grupo_controller_test.rb
@@ -4,9 +4,10 @@ require 'test_helper'
 class GruposControllerTest < ActionController::TestCase
 
   test "debería routear a descripcion" do
-    assert_generates '/grupos/ajax/descripcion', {  controller: 'grupos',
-                                                    action: 'ajax',
-                                                    atributo: 'descripcion' }
+    assert_generates '/grupos/autocompletar/descripcion',
+                      { controller: 'grupos',
+                        action: 'autocompletar',
+                        atributo: 'descripcion' }
   end
 
 end
