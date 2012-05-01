fb4a1c1aab23db1ad732fcd3eefce95f3c179f82
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-05-01 20:29:41 -0300

    configuración de paperclip en los adjuntos

--- app/models/adjunto.rb
+++ app/models/adjunto.rb
@@ -1,3 +1,5 @@
 class Adjunto < ActiveRecord::Base
   belongs_to :calicata, :inverse_of => :adjuntos
+  has_attached_file :archivo, { url: '/estaticos/:id/:filename',
+                                path: Rails.configuration.adjunto_path }
 end
