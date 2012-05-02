e9a556d081c8f47363e731bb85c75c7e6c8c6077
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-05-02 04:45:28 -0300

    arreglos en la tabla de archivos adjuntos

--- app/helpers/adjuntos_helper.rb
+++ app/helpers/adjuntos_helper.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 module AdjuntosHelper
   def icono_para_extension(ext)
-    "iconos/file_extension_#{ext}.png"
+    image_tag("iconos/file_extension_#{ext}.png", alt: ext)
   end
-end 
+end
