935c96d117a94d5c10296538898524a68c3943c5
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-05-01 21:23:01 -0300

    íconos según extensión en los adjuntos

---
+++ app/helpers/adjuntos_helper.rb
@@ -0,0 +1,6 @@
+# -*- encoding : utf-8 -*-
+module AdjuntosHelper
+  def icono_para_extension(ext)
+    "iconos/file_extension_#{ext}.png"
+  end
+end 
--- app/models/adjunto.rb
+++ app/models/adjunto.rb
@@ -2,4 +2,9 @@ class Adjunto < ActiveRecord::Base
   belongs_to :calicata, :inverse_of => :adjuntos
   has_attached_file :archivo, { url: '/estaticos/:id/:filename',
                                 path: Rails.configuration.adjunto_path }
+
+  def extension
+    File.extname(archivo.path).delete('.')
+  end
+
 end
