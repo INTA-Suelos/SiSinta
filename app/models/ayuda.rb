7452c9884ee816ac03c469ba71221776e7b57ca4
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-06-29 08:05:28 -0300

    arreglo la ayuda

--- app/models/ayuda.rb
+++ app/models/ayuda.rb
@@ -1,2 +1,9 @@
 class Ayuda < Lookup
+  include ActiveHash::Enum
+
+  # Lo declaro para que ActiveHash genere el finder
+  field :campo
+
+  # Garantiza que 'campo' sea único y genera constantes del tipo Ayuda::CAMPO
+  enum_accessor :campo
 end
