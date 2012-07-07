b62e22eb315166ffd9a21ec0ac5e510c0167bbf5
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-07-07 04:35:20 -0300

    no genero el <img /> si no hay una imagen

--- app/helpers/adjuntos_helper.rb
+++ app/helpers/adjuntos_helper.rb
@@ -1,6 +1,12 @@
-# -*- encoding : utf-8 -*-
+# encoding: utf-8
 module AdjuntosHelper
   def icono_para_extension(ext)
-    image_tag("iconos/file_extension_#{ext}.png", alt: ext)
+    archivo = "iconos/file_extension_#{ext}.png"
+
+    if File.exists? "app/assets/images/#{archivo}"
+      image_tag archivo, alt: ext
+    else
+      ".#{ext}"
+    end
   end
 end
