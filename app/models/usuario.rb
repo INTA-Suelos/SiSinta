6ae09ff82266d8abc5df3a6e9de99489af0b26a3
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-07-09 19:15:39 -0300

    scope para devolver sólo admins

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -14,6 +14,7 @@ class Usuario < ActiveRecord::Base
                   :remember_me, :ficha, :current_password
 
   scope :por_rol, joins(:roles).order('roles.nombre ASC')
+  scope :admins, joins(:roles).where('roles.nombre = ?', 'administrador')
 
   def to_s
     nombre
