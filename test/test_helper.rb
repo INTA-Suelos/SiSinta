30c6f0a2811fc7c5d3ac3c76caeff4e52e7377e6
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-09-03 19:59:50 -0300

    instalo factory_girl

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,15 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Helpers para los unit tests
 class ActiveSupport::TestCase
-  # Add more helper methods to be used by all tests here...
+  # Para llamar a los métodos core de FactoryGirl directamente (build,
+  # build_stubbed, create, attributes_for, y los *_list)
+  include FactoryGirl::Syntax::Methods
 end
 
+# Helpers para los controladores
 class ActionController::TestCase
-  # Helpers para los controladores
   include Devise::TestHelpers
 
   def loguearse_con_permisos
@@ -18,5 +21,4 @@ class ActionController::TestCase
   def json
     ActiveSupport::JSON.decode @response.body
   end
-
 end
