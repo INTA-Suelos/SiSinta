7132d541795f60ccae4640cb81b67e553770aaf5
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-09-07 06:05:25 -0300

    subo la version

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,7 @@ module SiSINTA
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '0.0.7'
+    config.assets.version = '0.0.8'
 
     # Traduzco el path
     config.assets.prefix = "/estaticos"
