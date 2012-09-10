bfc4f80cad413d797a7e2c6445cd7448ad8b8e17
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-09-10 02:11:23 -0300

    update de rails

--- app/decorators/calicata_decorator.rb
+++ app/decorators/calicata_decorator.rb
@@ -5,7 +5,6 @@ class CalicataDecorator < Draper::Base
   def fecha
     calicata.fecha.to_s(:dma)
   end
-  alias_method :fecha_before_type_cast, :fecha
 
   def etiquetas
     calicata.etiquetas.join(', ')
