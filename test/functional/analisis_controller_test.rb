71d420f85bfef1a7c37b964ad590f5b530792137
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-02-18 11:08:30 -0300

    Adjuntos y Analisis con responders

--- test/functional/analisis_controller_test.rb
+++ test/functional/analisis_controller_test.rb
@@ -10,21 +10,27 @@ class AnalisisControllerTest < ActionController::TestCase
     @request.env["HTTP_REFERER"] = "/perfiles/#{@perfil.to_param}/analisis"
   end
 
-  test "should get index" do
+  test "debería ir al índice de análisis si está autorizado" do
     get :index, perfil_id: @perfil.id
     assert_response :success
     perfil = assigns(:perfil)
+    analisis = assigns(:analisis)
     assert_not_nil perfil
-    assert_not_nil perfil.analisis
+    assert_equal analisis.all.sort, perfil.analisis.sort
   end
 
-  test "should get edit" do
+  test "debería ir a 'editar' si está autorizado" do
     get :edit, id: @analisis.to_param, perfil_id: @perfil.id
     assert_response :success
+    assert_not_nil assigns(:perfil)
+    assert_not_nil assigns(:analisis)
   end
 
-  test "should update analisis" do
+  test "debería actualizar los análisis" do
     put :update, id: @analisis.to_param, analisis: @analisis.attributes, perfil_id: @perfil.id
+    assert_not_nil assigns(:perfil)
+    assert_not_nil assigns(:analisis)
+
     assert_redirected_to perfil_analisis_index_path(@perfil)
   end
 
