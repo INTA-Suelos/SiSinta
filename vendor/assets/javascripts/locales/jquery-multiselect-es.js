d189a67c8de28d176438100a5c8d463b64190c67
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-02-20 18:59:45 -0300

    Traducción en español del multiselect

--- vendor/assets/javascripts/locales/jquery-multiselect-es.js
+++ vendor/assets/javascripts/locales/jquery-multiselect-es.js
@@ -10,41 +10,40 @@
  * http://mind2soft.com/labs/jquery/multiselect/
  *
  *
- * Localization : EN
+ * Localization : ES
  *
  */
 
 (function($) {
 
-    $.uix.multiselect.i18n['en'] = {
-        itemsSelected_nil: 'no selected option',           // 0
-        itemsSelected: '{count} selected option',          // 0, 1
-        itemsSelected_plural: '{count} selected options',  // n
-        //itemsSelected_plural_two: ...                    // 2
-        //itemsSelected_plural_few: ...                    // 3, 4
-        itemsAvailable_nil: 'no item available',
-        itemsAvailable: '{count} available option',
-        itemsAvailable_plural: '{count} available options',
+    $.uix.multiselect.i18n['es'] = {
+        itemsSelected_nil: 'no hay opciones seleccionadas',     // 0
+        itemsSelected: '{count} opción seleccionada',           // 0, 1
+        itemsSelected_plural: '{count} opciones seleccionadas', // n
+        //itemsSelected_plural_two: ...                         // 2
+        //itemsSelected_plural_few: ...                         // 3, 4
+        itemsAvailable_nil: 'no hay opciones disponibles',
+        itemsAvailable: '{count} opción disponible',
+        itemsAvailable_plural: '{count} opciones disponibles',
         //itemsAvailable_plural_two: ...
         //itemsAvailable_plural_few: ...
-        itemsFiltered_nil: 'no option filtered',
-        itemsFiltered: '{count} option filtered',
-        itemsFiltered_plural: '{count} options filtered',
+        itemsFiltered_nil: 'ninguna opción filtrada',
+        itemsFiltered: '{count} opción filtrada',
+        itemsFiltered_plural: '{count} opciones filtradas',
         //itemsFiltered_plural_two: ...
         //itemsFiltered_plural_few: ...
-        selectAll: 'Select All',
-        deselectAll: 'Deselect All',
-        search: 'Search Options',
-        collapseGroup: 'Collapse Group',
-        expandGroup: 'Expand Group',
-        selectAllGroup: 'Select All Group',
-        deselectAllGroup: 'Deselect All Group'
+        selectAll: 'Seleccionar Todo',
+        deselectAll: 'Deseleccionar Todo',
+        search: 'Buscar opciones',
+        collapseGroup: 'Plegar Grupo',
+        expandGroup: 'Expandir Grupo',
+        selectAllGroup: 'Seleccionar Grupo Entero',
+        deselectAllGroup: 'Deseleccionar Grupo Entero'
     };
 
     // link locales
-    $.uix.multiselect.i18n['en_CA'] = $.uix.multiselect.i18n['en'];
-    $.uix.multiselect.i18n['en_GB'] = $.uix.multiselect.i18n['en'];
-    $.uix.multiselect.i18n['en_US'] = $.uix.multiselect.i18n['en'];
+    $.uix.multiselect.i18n['es_ES'] = $.uix.multiselect.i18n['es'];
+    $.uix.multiselect.i18n['es_AR'] = $.uix.multiselect.i18n['es'];
     // ...
 
 })(jQuery);
