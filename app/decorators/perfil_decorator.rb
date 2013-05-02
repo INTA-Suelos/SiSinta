099d7b11788b1b9c422efee2898ba626a345f0d1
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-05-02 19:03:06 -0300

    Preparar asociaciones en el decorador

--- app/decorators/perfil_decorator.rb
+++ app/decorators/perfil_decorator.rb
@@ -4,6 +4,7 @@ class PerfilDecorator < ApplicationDecorator
   decorates_association :serie
   decorates_association :proyecto
   decorates_association :fase
+  decorates_association :grupo
   decorates_association :adjuntos, with: PaginadorDecorator
   decorates_association :analiticos
   decorates_association :usuario
@@ -22,5 +23,28 @@ class PerfilDecorator < ApplicationDecorator
 
   def to_s
     source.numero || source.nombre
+  end
+
+  # Para el +FormHelper+ necesito los objetos instanciados, aunque no tengan
+  # asociaciones realizadas, asique acá les asignamos un objeto nuevo si no
+  # tenían ya
+  def preparar
+    source.serie         ||= Serie.new
+    source.grupo         ||= Grupo.new
+    source.paisaje       ||= Paisaje.new
+    source.capacidad     ||= Capacidad.new
+    source.fase          ||= Fase.new
+    source.ubicacion     ||= Ubicacion.new
+    source.humedad       ||= Humedad.new
+    source.pedregosidad  ||= Pedregosidad.new
+    source.erosion       ||= Erosion.new
+    source.horizontes.each do |h|
+      h.color_seco    || h.build_color_seco
+      h.color_humedo  || h.build_color_humedo
+      h.limite        || h.build_limite
+      h.consistencia  || h.build_consistencia
+      h.estructura    || h.build_estructura
+    end
+    self
   end
 end
--- app/helpers/perfiles_helper.rb
+++ app/helpers/perfiles_helper.rb
@@ -12,29 +12,6 @@ module PerfilesHelper
   # Para el +FormHelper+ necesito los objetos instanciados, aunque no tengan
   # asociaciones realizadas, asique acá les asignamos un objeto nuevo si no
   # tenían ya
-  def perfil_preparado
-    @perfil.serie         ||= Serie.new
-    @perfil.grupo         ||= Grupo.new
-    @perfil.paisaje       ||= Paisaje.new
-    @perfil.capacidad     ||= Capacidad.new
-    @perfil.fase          ||= Fase.new
-    @perfil.ubicacion     ||= Ubicacion.new
-    @perfil.humedad       ||= Humedad.new
-    @perfil.pedregosidad  ||= Pedregosidad.new
-    @perfil.erosion       ||= Erosion.new
-    @perfil.horizontes.each do |h|
-      h.color_seco    || h.build_color_seco
-      h.color_humedo  || h.build_color_humedo
-      h.limite        || h.build_limite
-      h.consistencia  || h.build_consistencia
-      h.estructura    || h.build_estructura
-    end
-    @perfil
-  end
-
-  # Para el +FormHelper+ necesito los objetos instanciados, aunque no tengan
-  # asociaciones realizadas, asique acá les asignamos un objeto nuevo si no
-  # tenían ya
   def horizonte_preparado
     Horizonte.new(
       profundidad_superior:
