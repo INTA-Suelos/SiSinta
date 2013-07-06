c23f75ee805c1159ec49cab78045ba43e4f99cc7
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-07-05 22:58:06 -0300

    Cierra #21

--- app/models/erosion.rb
+++ app/models/erosion.rb
@@ -8,6 +8,7 @@ class Erosion < ActiveRecord::Base
   has_lookup :subclase, inverse_of: :erosiones, class_name: 'SubclaseDeErosion'
 
   validates_presence_of :perfil
+  validate :acumulacion_sin_clase
   delegate :publico, :usuario, :usuario_id, to: :perfil
 
   # TODO A un decorator
@@ -18,4 +19,13 @@ class Erosion < ActiveRecord::Base
   def self.ransackable_attributes(auth_object = nil)
     super(auth_object) - ['created_at', 'updated_at', 'perfil_id', 'id']
   end
+
+  private
+
+    # Valida que si se selecciona 'acumulación' no se cargue una clase
+    def acumulacion_sin_clase
+      if subclase == SubclaseDeErosion.find_by_valor('acumulación') and clase.present?
+        errors.add :clase, :debe_estar_en_blanco
+      end
+    end
 end
