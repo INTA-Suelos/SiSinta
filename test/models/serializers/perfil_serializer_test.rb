8969b8274bc3dc9c9666d4ec068e84f1443f70c5
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-07-29 19:22:36 -0300

    Testear serializer por separado

---
+++ test/models/serializers/perfil_serializer_test.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+require './test/test_helper'
+
+class PerfilSerializerTest < ActiveSupport::TestCase
+  test 'serializa todos los atributos' do
+    perfil = create(:perfil_completo)
+    hash = PerfilSerializer.new(perfil).serializable_hash
+
+    hash[:id].must_equal perfil.id
+    hash[:fecha].must_equal perfil.fecha
+    hash[:numero].must_equal perfil.numero
+    hash[:modal].must_equal perfil.modal
+    hash[:profundidad_napa].must_equal perfil.profundidad_napa
+    hash[:cobertura_vegetal].must_equal perfil.cobertura_vegetal
+    hash[:material_original].must_equal perfil.material_original
+    hash[:vegetacion_o_cultivos].must_equal perfil.vegetacion_o_cultivos
+    hash[:observaciones].must_equal perfil.observaciones
+
+    # Lookups
+    %i{ drenaje relieve posicion pendiente escurrimiento permeabilidad
+        anegamiento uso_de_la_tierra }.each do |lookup|
+      hash[lookup].must_equal LookupSerializer.new(perfil.send(lookup)).serializable_hash
+    end
+    hash[:sales].must_equal LookupSerializer.new(perfil.sal).serializable_hash
+
+    hash[:usuario].blank?.must_equal true
+    hash[:publico].blank?.must_equal true
+    hash[:created_at].blank?.must_equal true
+    hash[:updated_at].blank?.must_equal true
+  end
+end
