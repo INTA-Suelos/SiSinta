1111a2cd4a5cd80c82200b300bf32acf19d51e70
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-08-11 22:20:48 -0300

    Guardar la ficha en la sesión para la siguiente solicitud

--- app/controllers/perfiles_controller.rb
+++ app/controllers/perfiles_controller.rb
@@ -29,6 +29,8 @@ class PerfilesController < AutorizadoController
     o.before_filter :ordenar
   end
 
+  before_filter :seleccionar_ficha, only: [:edit, :new]
+
   before_filter :buscar_perfiles_o_exportar,    only: [:procesar]
   before_filter :cargar_perfiles_seleccionados, only: [:exportar, :procesar]
 
@@ -228,6 +230,8 @@ class PerfilesController < AutorizadoController
       when t('helpers.submit.perfil.analiticos')
         editar_analiticos_perfil_path(@perfil)
       when t('helpers.submit.perfil.cambiar_ficha')
+        # Un filter carga @ficha con este valor en la siguiente solicitud
+        session[:ficha] = params[:ficha]
         edit_perfil_path(@perfil)
       else
         @perfil
@@ -291,5 +295,9 @@ class PerfilesController < AutorizadoController
 
     def marcado_para_remover?(hash)
       hash[:_destroy].present? or hash[:anular].present?
+    end
+
+    def seleccionar_ficha
+      @ficha = session.delete(:ficha) || current_usuario.ficha
     end
 end
