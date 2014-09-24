9647e40b2df7517608c072575db6123a102dae91
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-09-24 03:38:49 -0300

    Mantener al usuario logueado en los tests de integración

--- test/factories/usuarios.rb
+++ test/factories/usuarios.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :usuario do
     nombre { generate :cadena_unica }
     email
-    password "algún password inolvidable"
+    password 'algún password inolvidable'
 
     ignore { rol nil }
     after(:build) do |usuario, params|
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,11 @@
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'minitest/rails'
 require 'minitest/mock'
+require 'minitest/rails'
 require 'minitest/rails/capybara'
 
 DatabaseCleaner.clean_with :truncation
-DatabaseCleaner.strategy = :transaction
 
 # Helpers para los unit tests
 class ActiveSupport::TestCase
@@ -20,8 +19,8 @@ end
 class ActionController::TestCase
   include Devise::TestHelpers
 
-  before { DatabaseCleaner.start }
-  after { DatabaseCleaner.clean }
+  setup { DatabaseCleaner.start }
+  teardown { DatabaseCleaner.clean }
 
   def loguearse
     loguearse_como 'Cualquiera'
@@ -50,37 +49,35 @@ class ActionController::TestCase
   end
 end
 
-# Capybara is intended to be used for automating a browser to test your
-# application’s features. This is different than the integration tests that
-# Rails provides, so you must use the Capybara::Rails::TestCase for your
-# feature tests.
+# Tests de integración
 class Capybara::Rails::TestCase
   include ApplicationHelper
   include Warden::Test::Helpers # login_as
 
-  # Transactional fixtures do not work with Selenium tests, because Capybara
-  # uses a separate server thread, which the transactions would be hidden
-  # from. We hence use DatabaseCleaner to truncate our test database.
+  # No podemos usar transacciones con selenium
   DatabaseCleaner.strategy = :truncation
-  # Stop ActiveRecord from wrapping tests in transactions
   self.use_transactional_fixtures = false
 
   teardown do
     DatabaseCleaner.clean       # Truncate the database
     Capybara.reset_sessions!    # Forget the (simulated) browser state
     Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
+    # Limpiar los hooks de warden para todos los request (ver +loguearse_como+)
+    Warden::Manager._on_request.clear
+    Warden.test_reset!
   end
 
   def loguearse_como(tipo_de_usuario)
-    @usuario = create :usuario, rol: tipo_de_usuario
-    # http://www.10hacks.com/rspec-capybara-devise-login-tests/
-    login_as @usuario, scope: :usuario
-    return @usuario
+    loguear create(:usuario, rol: tipo_de_usuario)
   end
 
-  # Datos requeridos para submitear un perfil
-  def completar_datos_de_perfil_requeridos
-    fill_in Perfil.human_attribute_name('fecha'),    with: '23/3/1987'
+  def loguear(usuario)
+    # Este debería ser el default de Warden
+    Warden::Manager.on_request do |proxy|
+      proxy.set_user usuario, scope: :usuario
+    end
+
+    usuario
   end
 end
 
