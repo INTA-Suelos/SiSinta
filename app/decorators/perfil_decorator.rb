7854a9ee2022544b3a6b4a18662a9ac5da113548
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-10-01 06:46:04 -0300

    Devolver perfil.clase (grupo + fase) gracias a los decoradores y null object

--- app/decorators/perfil_decorator.rb
+++ app/decorators/perfil_decorator.rb
@@ -3,8 +3,6 @@ class PerfilDecorator < ApplicationDecorator
   decorates_association :ubicacion
   decorates_association :serie
   decorates_association :proyecto
-  decorates_association :fase
-  decorates_association :grupo
   decorates_association :adjuntos, with: PaginadorDecorator
   decorates_association :analiticos
   decorates_association :usuario
@@ -55,5 +53,18 @@ class PerfilDecorator < ApplicationDecorator
 
   def link_a_serie
     h.link_to nombre, serie, class: 'perfil_nombre' if serie.present?
+  end
+
+  def grupo
+    GrupoDecorator.new source.try(:grupo)
+  end
+
+  def fase
+    FaseDecorator.new source.try(:fase)
+  end
+
+  # En el GeoJSON le decimos `clase` a grupo + fase
+  def clase
+    [grupo.descripcion, fase.nombre].join(' ').strip
   end
 end
