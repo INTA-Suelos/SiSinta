87303aa5977c18719bdf423bc75f1c8ef9f9c953
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2014-12-12 12:31:27 -0300

    Guardar adjuntos donde está capistrano

--- app/models/adjunto.rb
+++ app/models/adjunto.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class Adjunto < ActiveRecord::Base
   belongs_to :perfil, inverse_of: :adjuntos
-  has_attached_file :archivo, { url: '/estaticos/:id/:filename',
+  has_attached_file :archivo, { url: '/system/:class/:id/:filename',
                                 path: Rails.configuration.adjunto_path }
 
   validates_attachment :archivo, file_type_ignorance: true
