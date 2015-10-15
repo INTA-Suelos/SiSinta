fc5f8857954b9ac3d11a82e1402b1e9514f6f9f5
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2015-10-15 01:28:32 -0300

    tests de UbicacionDecorator

--- test/decorators/ubicacion_decorator_test.rb
+++ test/decorators/ubicacion_decorator_test.rb
@@ -1,30 +1,29 @@
 # encoding: utf-8
 require './test/test_helper'
 
-class UbicacionDecoratorTest < Draper::TestCase
-  def setup
-    @lugar = build_stubbed :ubicacion, x: '-35.0', y: '-35.0'
-  end
+describe UbicacionDecorator do
+  let(:lugar) { build_stubbed :ubicacion, x: '-35.0', y: '-35.0' }
+  let(:usuario) { usuario = build_stubbed :usuario, config: { srid: 22195 } }
+  let(:lugar_decorado) { UbicacionDecorator.decorate lugar, context: { usuario: usuario } }
 
-  test "devuelve coordenadas por default si no hay usuario logueado" do
-    # "Stub" de método inexistente (singleton method)
+  it 'devuelve coordenadas por default si no hay usuario logueado' do
+    # 'Stub' de método inexistente (singleton method)
+    # Necesario porque el decorador busca en current_usuario si no hay contexto
     def helpers.current_usuario
       nil
     end
 
-    assert_equal 4326, UbicacionDecorator.decorate(@lugar).srid
+    UbicacionDecorator.decorate(lugar).srid.must_equal 4326
   end
 
-  test "devuelve las coordenadas proyectadas según las preferencias" do
-    usuario = build_stubbed :usuario, config: { srid: 22195 }
-    lugar_decorado = UbicacionDecorator.decorate(@lugar, context: { usuario: usuario })
+  it 'devuelve las coordenadas proyectadas según las preferencias' do
+    lugar_decorado.srid.must_equal usuario.srid
+  end
 
-    assert_equal usuario.srid, lugar_decorado.srid
+  it 'proyecta x e y de acuerdo al srid preferido' do
+    proyeccion = Ubicacion.transformar(lugar.srid, usuario.srid, lugar.x, lugar.y)
 
-    proyeccion = Ubicacion.transformar(@lugar.srid, usuario.srid, @lugar.x, @lugar.y)
-    assert_equal Ubicacion.redondear(proyeccion.x), lugar_decorado.x,
-      "x no coincide con la preferencia"
-    assert_equal Ubicacion.redondear(proyeccion.y), lugar_decorado.y,
-      "y no coincide con la preferencia"
+    lugar_decorado.x.must_equal Ubicacion.redondear(proyeccion.x)
+    lugar_decorado.y.must_equal Ubicacion.redondear(proyeccion.y)
   end
 end
