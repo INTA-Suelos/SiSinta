d579b5cc2451886434f3b44db3e37da12675a004
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2015-10-15 00:44:44 -0300

    tests de ApplicationDecorator

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -13,6 +13,7 @@ class ApplicationDecorator < Draper::Decorator
   # Convierte el recurso en array, de acuerdo al filtro o por default todos los
   # atributos. Acepta otros decoradores y cualquier método al que responda el
   # objeto.
+  # TODO Dónde se usa?
   def to_array(filtro = nil)
     filtro ||= attributes.keys.flatten
     filtro.inject([]) do |arreglo, atributo|
