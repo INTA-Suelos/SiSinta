04b4ce5d1f3c7778ffbe48bb6853c6863b13583c
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2015-10-19 01:20:41 -0300

    Usar nueva configuración en vez de monkeypatch

--- app/models/adjunto.rb
+++ app/models/adjunto.rb
@@ -1,8 +1,12 @@
 # encoding: utf-8
 class Adjunto < ActiveRecord::Base
   belongs_to :perfil, inverse_of: :adjuntos
-  has_attached_file :archivo, { url: '/system/:class/:id/:filename',
-                                path: Rails.configuration.adjunto_path }
+  has_attached_file :archivo, {
+    url: '/system/:class/:id/:filename',
+    path: Rails.configuration.adjunto_path,
+    # No verificar que el tipo de archivo coincida con la extensión
+    validate_media_type: false
+  }
 
   validates_attachment :archivo, file_type_ignorance: true
 
--- test/models/adjunto_test.rb
+++ test/models/adjunto_test.rb
@@ -3,6 +3,11 @@ require './test/test_helper'
 
 class AdjuntoTest < ActiveSupport::TestCase
 
+  setup do
+    @perfil = create(:perfil)
+    @adjunto = create(:adjunto, perfil: @perfil)
+  end
+
   test 'está configurado' do
     assert_not_nil Rails.configuration.adjunto_path
   end
@@ -13,11 +18,14 @@ class AdjuntoTest < ActiveSupport::TestCase
   end
 
   test 'delega publico a perfil' do
-    perfil = create(:perfil)
-    adjunto = create(:adjunto, perfil: perfil)
-    assert_equal perfil.publico, adjunto.publico
-    perfil.publico = !perfil.publico
-    assert_equal perfil.publico, adjunto.publico
+    assert_equal @perfil.publico, @adjunto.publico
+    @perfil.publico = !@perfil.publico
+    assert_equal @perfil.publico, @adjunto.publico
   end
 
+  test 'no valida el media type' do
+    falso = Rack::Test::UploadedFile.new Rails.root.join('test', 'data', 'falso.jpg'), 'image/png'
+
+    build(:adjunto, perfil: @perfil, archivo: falso).must_be :valid?
+  end
 end
