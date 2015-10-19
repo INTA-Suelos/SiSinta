acdcd3d451db4eba4605bc7889a85b5c3a00b49e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2015-10-19 03:40:39 -0300

    Usar spec style en test de ability y analitico

--- test/models/analitico_test.rb
+++ test/models/analitico_test.rb
@@ -1,25 +1,12 @@
 # encoding: utf-8
 require './test/test_helper'
 
-class AnaliticoTest < ActiveSupport::TestCase
+describe Analitico do
 
-  setup do
-    @analitico = build_stubbed(:analitico)
-  end
+  let(:analitico) { build_stubbed(:analitico) }
 
-  test 'devuelve materia_organica_cn si no es null' do
-    @analitico.materia_organica_cn = 4
-    @analitico.materia_organica_n = 1.1
-    @analitico.materia_organica_c = 1.1
-    assert_equal 4, @analitico.materia_organica_cn
-  end
-
-  test 'permite asignación masiva de ciertos atributos' do
-    assert Analitico.create(attributes_for(:analitico))
-  end
-
-  test 'usa precisión 000.00 en los porcentajes' do
-    a = build(:analitico, horizonte: build(:horizonte),
+  it 'usa precisión 000.00 en los porcentajes' do
+    a = create(:analitico, horizonte: build(:horizonte),
       humedad: 99.98,
       arcilla: 0.01,
       materia_organica_c: 100,
@@ -37,52 +24,43 @@ class AnaliticoTest < ActiveSupport::TestCase
       saturacion_s_h: 0.15,
       agua_3_atm: 0.16 )
 
-    assert a.valid?
-    a.save
-    a.reload
-    assert_equal 99.98, a.humedad
-    assert_equal 0.01, a.arcilla
-    assert_equal 100, a.materia_organica_c
-    assert_equal 0.04, a.limo_2_20
-    assert_equal 100, a.limo_2_50
-    assert_equal 0.06, a.arena_muy_fina
-    assert_equal 100.00, a.arena_fina
-    assert_equal 0.08, a.arena_media
-    assert_equal 0.09, a.arena_gruesa
-    assert_equal 0.10, a.arena_muy_gruesa
-    assert_equal 0.11, a.ca_co3
-    assert_equal 0.12, a.agua_15_atm
-    assert_equal 0.13, a.agua_util
-    assert_equal 0.14, a.saturacion_t
-    assert_equal 0.15, a.saturacion_s_h
-    assert_equal 0.16, a.agua_3_atm
+    a.humedad.must_equal 99.98
+    a.arcilla.must_equal 0.01
+    a.materia_organica_c.must_equal 100
+    a.limo_2_20.must_equal 0.04
+    a.limo_2_50.must_equal 100
+    a.arena_muy_fina.must_equal 0.06
+    a.arena_fina.must_equal 100.00
+    a.arena_media.must_equal 0.08
+    a.arena_gruesa.must_equal 0.09
+    a.arena_muy_gruesa.must_equal 0.10
+    a.ca_co3.must_equal 0.11
+    a.agua_15_atm.must_equal 0.12
+    a.agua_util.must_equal 0.13
+    a.saturacion_t.must_equal 0.14
+    a.saturacion_s_h.must_equal 0.15
+    a.agua_3_atm.must_equal 0.16
   end
 
-  test 'usa precisión 000.000 para materia_organica_n' do
-    a = build(:analitico, horizonte: build(:horizonte),
-      materia_organica_n: 0.004 )
+  describe '#materia_organica_cn' do
+    it 'usa precisión 000.000' do
+      a = create(:analitico, horizonte: build(:horizonte),
+        materia_organica_n: 0.004 )
 
-    assert a.valid?
-    a.save
-    a.reload
-    assert_equal 0.004, a.materia_organica_n.to_f
-
-    a.update_attribute('materia_organica_n', 100)
-    a.reload
-    assert_equal 100, a.materia_organica_n
+      a.reload.materia_organica_n.to_f.must_equal 0.004
+    end
   end
 
-  test 'usa precisión 000000000000000000.0 en materia_organica_cn' do
-    a = build(:analitico, horizonte: build(:horizonte),
-      materia_organica_cn: 0.3 )
+  describe '#materia_organica_cn' do
+    it 'usa precisión 000000000000000000.0' do
+      a = create(:analitico, horizonte: build(:horizonte),
+        materia_organica_cn: 0.3)
 
-    assert a.valid?
-    a.save
-    a.reload
-    assert_equal 0.3, a.materia_organica_cn
+      a.materia_organica_cn.must_equal 0.3
 
-    a.update_attribute('materia_organica_cn', 999999999999999999.9)
-    a.reload
-    assert_equal 999999999999999999.9, a.materia_organica_cn.to_f
+      a.update_attribute :materia_organica_cn, 999999999999999999.9
+
+      a.reload.materia_organica_cn.to_f.must_equal 999999999999999999.9
+    end
   end
 end
