48faf1580f6502d7ef5cd7e828bd5365157bdb4c
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2015-11-03 00:07:00 -0300

    Tests de Consistencia spec style

--- test/models/consistencia_test.rb
+++ test/models/consistencia_test.rb
@@ -1,30 +1,22 @@
-# encoding: utf-8
 require './test/test_helper'
 
-class ConsistenciaTest < ActiveSupport::TestCase
+describe Consistencia do
+  let(:consistencia) { build_stubbed(:consistencia) }
 
-  setup do
-    @consistencia = build_stubbed(:consistencia)
-  end
-
-  test "accede a sus asociaciones" do
-    assert @consistencia.respond_to? :en_seco, "falla en_seco"
-    assert @consistencia.respond_to? :en_humedo, "falla en_humedo"
-    assert @consistencia.respond_to? :adhesividad, "falla adhesividad"
-    assert @consistencia.respond_to? :plasticidad, "falla plasticidad"
-    assert_nothing_raised do
-      @consistencia.en_seco
-      @consistencia.en_humedo
-      @consistencia.adhesividad
-      @consistencia.plasticidad
+  it 'accede a sus asociaciones' do
+    %i{en_seco en_humedo adhesividad plasticidad horizonte}.each do |asociacion|
+      consistencia.respond_to?(asociacion).must_equal true, "No responde a #{asociacion}"
     end
-    assert @consistencia.respond_to? :horizonte
-
-    # Pruebo sus lookups
-    assert ConsistenciaEnSeco.first.respond_to? :consistencias
-    assert ConsistenciaEnHumedo.first.respond_to? :consistencias
-    assert AdhesividadDeConsistencia.first.respond_to? :consistencias
-    assert PlasticidadDeConsistencia.first.respond_to? :consistencias
   end
 
+  it 'se puede acceder a través de los lookups' do
+    [
+      ConsistenciaEnSeco,
+      ConsistenciaEnHumedo,
+      AdhesividadDeConsistencia,
+      PlasticidadDeConsistencia
+    ].each do |lookup|
+      lookup.first.respond_to?(:consistencias).must_equal true, "No se puede acceder a través de #{lookup.name}"
+    end
+  end
 end
