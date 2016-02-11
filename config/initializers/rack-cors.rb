a881cc6f7777af24e5b3a3571266633d10e915a8
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-02-11 17:10:52 -0300

    Permitir requests desde otro dominio

---
+++ config/initializers/rack-cors.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :options]
+  end
+end
