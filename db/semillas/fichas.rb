33c2a29ec3121bf35d22ac0f0fb048874fece22d
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-05-05 21:35:40 -0300

    Ficha de corrientes en la lista (termina #108)

--- db/semillas/fichas.rb
+++ db/semillas/fichas.rb
@@ -1,7 +1,7 @@
 # Las diferentes plantillas de carga que entiende el sistema
 [
-  { nombre: 'Formulario clásico de Etchevere',
-  identificador: 'completa' }
+  { nombre: 'Formulario clásico de Etchevere', identificador: 'clasico' },
+  { nombre: 'Formulario Provincia de Corrientes', identificador: 'corrientes' }
 ].each do |ficha|
   Ficha.find_or_create_by nombre: ficha[:nombre], identificador: ficha[:identificador]
 end
