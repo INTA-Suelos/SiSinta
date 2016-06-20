31d21d679551d1af07a24b937144d76ddbbb7600
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-06-20 02:50:41 -0300

    [style] Sintaxis vieja y comillas

--- test/models/helpers/application_helper_test.rb
+++ test/models/helpers/application_helper_test.rb
@@ -1,4 +1,3 @@
-# encoding: utf-8
 require './test/test_helper'
 require 'application_helper'
 
@@ -9,18 +8,18 @@ class ApplicationHelperTest < ActionView::TestCase
     @a = Ayuda.first
   end
 
-  test "genera un div simple" do
+  test 'genera un div simple' do
     render text: ayuda_para(@a.campo)
     assert_select "div#ayuda_#{@a.campo.gsub('.', '_')}.ayuda", {count: 1, text: @a.ayuda}
   end
 
-  test "genera un div con las clases especificadas" do
-    render text: ayuda_para(@a.campo, {:clases => 'uno dos'})
+  test 'genera un div con las clases especificadas' do
+    render text: ayuda_para(@a.campo, { clases: 'uno dos'})
     assert_select 'div.uno.dos', 1
   end
 
-  test "genera un div con el id especificado" do
-    render text: ayuda_para(@a.campo, {:id => 'algo'})
+  test 'genera un div con el id especificado' do
+    render text: ayuda_para(@a.campo, { id: 'algo'})
     assert_select 'div#algo.ayuda', 1
   end
 end
