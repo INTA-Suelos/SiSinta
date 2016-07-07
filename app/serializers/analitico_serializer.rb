91c8d81c5f69f822c5e9eea430ef1c20e874763b
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-07-06 21:49:00 -0300

    Exportar gravas y arena_total en el CSV

--- app/serializers/analitico_serializer.rb
+++ app/serializers/analitico_serializer.rb
@@ -3,8 +3,8 @@ class AnaliticoSerializer < ActiveModel::Serializer
               :resistencia_pasta, :base_ca, :base_mg, :base_k, :base_na,
               :arcilla, :carbono_organico_c, :carbono_organico_n, :limo_2_20,
               :limo_2_50, :arena_muy_fina, :arena_fina, :arena_media,
-              :arena_gruesa, :arena_muy_gruesa, :ca_co3, :agua_15_atm,
-              :agua_util, :conductividad, :h, :saturacion_t, :saturacion_s_h,
-              :densidad_aparente, :profundidad_muestra, :agua_3_atm,
-              :carbono_organico_cn, :base_al, :p_ppm
+              :arena_gruesa, :arena_muy_gruesa, :arena_total, :gravas, :ca_co3,
+              :agua_15_atm, :agua_util, :conductividad, :h, :saturacion_t,
+              :saturacion_s_h, :densidad_aparente, :profundidad_muestra,
+              :agua_3_atm, :carbono_organico_cn, :base_al, :p_ppm
 end
