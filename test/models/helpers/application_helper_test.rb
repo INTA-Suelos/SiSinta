9917bc9a3faf83dd403e64efd8b05fb896fde33f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-07-12 05:00:58 -0300

    Actualizo el test

--- test/models/helpers/application_helper_test.rb
+++ test/models/helpers/application_helper_test.rb
@@ -1,21 +1,21 @@
 require './test/test_helper'
-require 'application_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  include ApplicationHelper
-
-  test 'genera un div simple' do
+  it 'genera un div simple' do
     render text: ayuda_para('este texto')
-    assert_select 'div.ayuda', { count: 1, text: 'este texto' }
+
+    must_select 'div.ayuda', { count: 1, text: 'este texto' }
   end
 
-  test 'genera un div con las clases especificadas' do
+  it 'genera un div con las clases especificadas' do
     render text: ayuda_para('algo', { clases: 'uno dos' })
-    assert_select 'div.uno.dos', 1
+
+    must_select 'div.uno.dos', 1
   end
 
-  test 'genera un div con el id especificado' do
+  it 'genera un div con el id especificado' do
     render text: ayuda_para('algo', { id: 'especificado' })
-    assert_select 'div#especificado.ayuda', 1
+
+    must_select 'div#especificado.ayuda', 1
   end
 end
