c7ea4e1f560840ac014bb44b1cfac14b54e3e17b
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-09-16 01:22:47 -0300

    Asociar provincias y datos del IGN

--- app/models/provincia.rb
+++ app/models/provincia.rb
@@ -10,4 +10,11 @@ class Provincia < Lookup
   enum_accessor :slug
 
   alias_attribute :nombre, :valor
+  # La foreign key de IgnProvincia
+  alias_attribute :gid, :ign_provincia_id
+
+  # Reemplazar por :belongs_to cuando convierta Provincia a ActiveRecord
+  def ign_provincia
+    IgnProvincia.find_by gid: ign_provincia_id
+  end
 end
