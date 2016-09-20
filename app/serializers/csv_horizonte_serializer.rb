4fb0744038e26e53059ca1996442322ca1945d82
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-09-20 01:13:28 -0300

    Nota sobre fix

--- app/serializers/csv_horizonte_serializer.rb
+++ app/serializers/csv_horizonte_serializer.rb
@@ -4,6 +4,10 @@ class CSVHorizonteSerializer < HorizonteSerializer
   def to_csv(columnas = [])
     lista = HashWithIndifferentAccess.new(self.serializable_hash).sort.flatten_tree
 
+    # FIXME Si por default usamos toda la lista, no podemos saber si al
+    # serializar se agregan columnas (capacidad, humedad) por lo que los
+    # headers quedan desfasados. Solución: generar un stub completísimo o no
+    # exportar nada si no se selecciona nada.
     if columnas.any?(&:present?)
       lista.select { |i| columnas.include? i }
     else
