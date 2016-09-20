e9af95d80712dc43e9dd8ad5fa39c0e72195646e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-09-20 00:47:12 -0300

    Nota sobre attribute_normalizer

--- config/initializers/attribute_normalizer.rb
+++ config/initializers/attribute_normalizer.rb
@@ -5,6 +5,7 @@ AttributeNormalizer.configure do |config|
   # You can change this if you would like as follows:
   # config.default_normalizers = :strip, :blank
 
+  # FIXME revisar los datos, nunca se aplicó esta configuración
   # You can enable the attribute normalizers automatically if the specified
   # attributes exist in your column_names. It will use
   # the default normalizers for each attribute (e.g.
