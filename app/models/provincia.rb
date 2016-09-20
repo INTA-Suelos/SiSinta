1a9db9bbae4664ddd06e67e8c4974cdb935cb27d
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-09-19 21:32:33 -0300

    Queries más generales

--- app/models/provincia.rb
+++ app/models/provincia.rb
@@ -13,17 +13,13 @@ class Provincia < Lookup
   # La foreign key de IgnProvincia
   alias_attribute :gid, :ign_provincia_id
 
-  # Reemplazar por :belongs_to cuando convierta Provincia a ActiveRecord
+  # FIXME Reemplazar por :belongs_to cuando convierta Provincia a ActiveRecord
   def ign_provincia
     IgnProvincia.find_by gid: ign_provincia_id
   end
 
   # Encuentra las ubicaciones dentro del área de esta provincia
   def ubicaciones
-    poligonos = IgnProvincia.arel_table[:geog]
-
-    Ubicacion.en_poligonos(poligonos)
-      .where(ign_provincias: { gid: gid })
-      .from('ubicaciones, ign_provincias')
+    Ubicacion.en_provincias(self.id)
   end
 end
--- app/models/ubicacion.rb
+++ app/models/ubicacion.rb
@@ -37,6 +37,22 @@ class Ubicacion < ActiveRecord::Base
     where(poligonos.st_intersects(puntos))
   end
 
+  # Encuentra las ubicaciones dentro de una o más provincias
+  def self.en_provincias(provincia_ids)
+    poligonos = IgnProvincia.arel_table[:geog]
+
+    # FIXME Reemplazar por query cuando convierta Provincia a ActiveRecord
+    #
+    #   gids = Provincia.select(:gid).where(id: provincia_ids)
+    #
+    # Tal vez pueda unificarla con el otro where
+    gids = Provincia.find(Array.wrap(provincia_ids)).map(&:ign_provincia_id)
+
+    Ubicacion.en_poligonos(poligonos)
+      .where(ign_provincias: { gid: gids })
+      .from('ubicaciones, ign_provincias')
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     super(auth_object) - ['created_at', 'updated_at', 'perfil_id', 'id']
   end
