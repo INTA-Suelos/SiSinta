90bd2e1c2bdba94eeea22494c910e46bbd2d8324
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-10-14 21:46:08 -0300

    Fixes

--- test/factories/fases.rb
+++ test/factories/fases.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 FactoryGirl.define do
   factory :fase do
-    codigo { generate :cadena_unica }
+    codigo { generate(:cadena_unica).slice(0, 2) }
     nombre { generate :cadena_unica }
   end
 end
