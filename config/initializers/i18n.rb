ef9566b5c72bb0925993c6343ded0d000dcf241e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-06-24 02:05:18 -0300

    Usar :es como fallback de :en

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,9 +1,14 @@
-# Buscar locales en subdirectorios de /config/locales
-I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+Rails.application.configure do
+  # Buscar locales en subdirectorios de /config/locales
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
-# Whitelistear los locales soportados
-I18n.available_locales = [:es, :en]
-I18n.enforce_available_locales = true
+  # Whitelistear los locales soportados
+  config.i18n.available_locales = [:es, :en]
+  config.i18n.enforce_available_locales = true
 
-# Usar español por default
-I18n.default_locale = :es
+  # Usar español por default
+  config.i18n.default_locale = :es
+
+  # Usar español como fallback
+  config.i18n.fallbacks = { en: :es }
+end
