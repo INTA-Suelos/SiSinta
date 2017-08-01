d6cb46bcf673b8501e5dbb5d8fc7f7497ea3cda1
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-08-01 02:04:04 -0300

    Descongelar versión de sass-rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,10 +1,14 @@
 Rails.application.configure do
-  # Precompile additional assets.
-  config.assets.precompile += %w( print.css )
-
   # Enable the asset pipeline
   config.assets.enabled = true
 
   # Version of your assets, change this if you want to expire all your assets
   config.assets.version = '0.4.7'
+
+  # Traduzco el path
+  config.assets.prefix = '/estaticos'
+
+  # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  config.assets.precompile += %w( ie.css print.css )
 end
