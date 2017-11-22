9984b53cb7ff225711f9de01c9c5b943d82553de
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-11-21 22:56:47 -0300

    El helper de elegir filas se inserta automáticamente

--- app/helpers/paginacion_helper.rb
+++ app/helpers/paginacion_helper.rb
@@ -1,9 +1,10 @@
-# encoding: utf-8
+# Helpers para cosas relacionadas con la barra de paginación genérica
 module PaginacionHelper
+  # Control para elegir la cantidad de filas a mostrar
   def elegir_filas(ruta, cantidades = %w{10 20 50})
     @cantidades = cantidades
 
-    content_tag(:div, id: 'filas') do
+    content_tag(:div, class: 'elegir_filas') do
       ('Mostrar ' +
       cantidades.collect do |cantidad|
         link_to cantidad, self.send("#{ruta}_path", filas: cantidad), class: activo?(cantidad)
@@ -12,9 +13,11 @@ module PaginacionHelper
     end
   end
 
-  # Para determinar el elemento activo de la paginación
+  # Devuelve la clase para link de elegir filas dependiendo de si el elemento
+  # está activo o no
   def activo?(elemento)
     activo = @cantidades.include?(params[:filas]) ? params[:filas] : '20'
+
     elemento == activo ? 'activo' : nil
   end
 end
