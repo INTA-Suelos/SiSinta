e021c47ba0f40ee90d1c0f262e4eaf4c2d44dcaa
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-03-06 23:43:31 +0100

    Extract deploy config to env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,16 @@
-set :deploy_user, 'sisar'
-set :deploy_to, '/srv/http/sisar.inta.gob.ar'
+set :deploy_user, ENV['DEPLOY_USER']
+set :deploy_to, ENV['DEPLOY_PATH']
 
-# Cómo subir la configuración de la app al server
-set :config_strategy, 'config:dir'
+# Which branch to deploy, master by default
+set :deploy_branch, ENV['DEPLOY_BRANCH']
+set :branch, fetch(:deploy_branch, :master)
 
-# Cómo reiniciar el webserver
+# How to provision server configuration files with capistrano-config_provider
+set :config_strategy, ENV['CONFIG_STRATEGY']
+set :config_repo_url, ENV['CONFIG_SOURCE']
+set :config_local_path, ENV['CONFIG_SOURCE']
+
+# How to restart the webserver
 set :passenger_restart_with_sudo, false
 
-set :branch, 'master'
-
-server 'sisar.inta.gob.ar', user: fetch(:deploy_user), roles: %w{app web db}
+server ENV['DEPLOY_SERVER'], user: fetch(:deploy_user), roles: %w{app web db}
