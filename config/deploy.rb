14fcc1f90918980e56b1371f7e6dcd3b6a34c373
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-03-06 23:43:31 +0100

    Extract deploy config to env

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
-# config valid only for current version of Capistrano
+# Config valid only for this version of Capistrano
 lock '3.10.1'
 
 set :application, 'SiSINTA'
 set :repo_url, 'https://github.com/INTA-Suelos/SiSinta.git'
 
-# Defaults de capistrano
+# Capistrano defaults
 set :branch, :master
 set :format, :pretty
 set :log_level, :debug
@@ -23,6 +23,9 @@ set :linked_dirs, %w{
   vendor/bundle
   public/system
 }
+
+# Configuration files adapted to the deploy server. You need to provision these
+# somehow
 set :linked_files, %w{
   config/secrets.yml
   config/database.yml
