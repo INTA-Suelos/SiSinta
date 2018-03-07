281d2eee2b92e65501ef90e683354d90a5b7e5ab
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-03-07 06:51:40 +0100

    Remove broken tasks

--- lib/recipes/cache.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-# Recetas para Capistrano relacionadas con el cache
-namespace :cache do
-  desc 'Para limpiar el cache después del deployment'
-  task :clear, :roles => :app do
-    run "cd #{current_release} && rake cache:clear RAILS_ENV=production"
-  end
-end
