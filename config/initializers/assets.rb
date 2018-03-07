0b81ebe1d9b825ffb34af411e98dae2d514874e4
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-03-07 06:51:53 +0100

    Use default assets dir

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,10 @@
 Rails.application.configure do
   # Precompile additional assets.
   config.assets.precompile += %w( print.css )
+
+  # Enable the asset pipeline
+  config.assets.enabled = true
+
+  # Version of your assets, change this if you want to expire all your assets
+  config.assets.version = '0.4.7'
 end
