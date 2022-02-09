48f54592dd50d83412f56a8d8d4a77600c8b9b47
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2022-02-09 05:56:07 -0300

    Tarea para reiniciar el servicio

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,3 +7,12 @@ set :passenger_restart_with_sudo, false
 server ENV['DEPLOY_SERVER'],
   user: fetch(:deploy_user),
   roles: %w{app web db}
+
+namespace :deploy do
+  desc 'Reiniciar servicio'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :sudo, 'systemctl restart sisinta.service'
+    end
+  end
+end
